b103c8dafcc17807c39b53f04764459941dd7099
unknown <54892@etu.he2b.be>
2020-03-27 14:24:53 +0100

    Etape 1

---
+++ data.js
@@ -0,0 +1,139 @@
+let data = [
+    { 
+        id: "ndls1", description: "Néerlandais de base",
+        questions: [{
+            question: "Je m'appelle Moussa",
+            answer: "Ik heet Moussa",
+            extras: "I hate"
+        },{
+            question: "J'ai 21 ans",
+            answer: "Ik ben 21 jaar",
+            extras: "Heeft jaren"
+        },{
+            question: "Il est trois heures",
+            answer: "Het is drie uur",
+            extras: "uren ben"
+        },{
+            question: "Comment vas-tu ?",
+            answer: "Hoe gaat het met je ?",
+            extras: "Who ga u"
+        }]
+    },{
+        id: "engl1", description: "Anglais pour débutant",
+        questions : [{
+            question: "Bonjour, comment allez-vous / vas-tu ?",
+            answer: "Hello, how are you ?",
+            extras: "do go"
+        },{
+            question: "Je suis désolé, je ne comprends pas.",
+            answer: "I’m sorry, I don’t understand.",
+            extras: "not you"
+        },{
+            question: "Où sont les toilettes ?",
+            answer: "Where are the bathroom ?",
+            extras: "Who"
+        },{
+            question: "Quelle heure est-il ?",
+            answer: "What time is it ?",
+            extras: "hour weather"
+        },{
+            question: "Je suis Belge, j’ai besoin d’aide.",
+            answer: "I’m Belgian, I need some help.",
+            extras: "Belgium want"
+        },{
+            question: "Je suis désolé de vous / te déranger.",
+            answer: "I’m sorry to bother you.",
+            extras: "of it"
+        },{
+            question: "Est-ce que vous pouvez répéter plus lentement ?",
+            answer: "Could you repeat that more slowly please ?",
+            extras: "for while"
+        },{
+            question: "Comment dit-on (...) en Anglais ?",
+            answer: "How do you say (...) in English ?",
+            extras: "Come on"
+        },{
+            question: "Je suis ici en vacances.",
+            answer: "I’m just here for holidays.",
+            extras: "vacancy in"
+        },{
+            question: "Est-ce que je peux avoir de l’eau svp ?",
+            answer: "Can I have some water, please ?",
+            extras: "few more"
+        }]
+    },{
+        id: "engl2", description: "Anglais pour voyager",
+        questions : [{
+            question: "Je cherche la Tour de Londres",
+            answer: "I’m looking for the Tower of London",
+            extras: "look I"
+        },{
+            question: "Est-ce que vous avez un plan de la ville ?",
+            answer: "Do you have a map of the town ?",
+            extras: "plan Have"
+        },{
+            question: "A quelle heure est le bus pour New-York ?",
+            answer: "At what time is the bus to New-York ?",
+            extras: "hour for"
+        },{
+            question: "Je voudrais réserver une chambre (d’hôtel) pour quatre jours",
+            answer: "I would like to book a room for four days",
+            extras: "reserve chamber"
+        },{
+            question: "Je voudrais louer une voiture pour une semaine",
+            answer: "I would like to rent a car for a week",
+            extras: "want"
+        },{
+            question: "Puis-je avoir de l’eau svp ?",
+            answer: "Could I have some water please ?",
+            extras: "a few"
+        },{
+            question: "Puis-je avoir l’addition ?",
+            answer: "May I have the bill ?",
+            extras: "addition check"
+        },{
+            question: "Combien ça coûte ? J’aimerais l’acheter.",
+            answer: "How much does it cost ? I would like to buy it",
+            extras: "many price sell"
+        },{
+            question: "Est-ce que je peux payer par carte ? Je n’ai pas d’espèces.",
+            answer: "Can I pay with credit card ? I don’t have cash.",
+            extras: "by map species money"
+        },{
+            question: "Où se trouve le supermarché ?",
+            answer: "Where can I find a supermarket ?",
+            extras: "is stand"
+        }]
+    },{
+        id: "engl3", description: "Anglais pour converser",
+        questions : [{
+            question: "Ravi de te rencontrer !",
+            answer: "Glad to meet you !",
+            extras: "encounter of"
+        },{
+            question: "J’espère te voir bientôt.",
+            answer: "Hope to see you soon.",
+            extras: "of that"
+        },{
+            question: "Merci pour ton (votre) aide.",
+            answer: "Many thanks for your help.",
+            extras: "of me"
+        },{
+            question: "De rien.",
+            answer: "You are welcome.",
+            extras: "Of nothing"
+        },{
+            question: "Je te (ou vous) demande pardon.",
+            answer: "I beg your pardon !",
+            extras: "you ask"
+        },{
+            question: "Je vous prie de m’excuser pour cette erreur.",
+            answer: "Please let me apologize for this mistake.",
+            extras: "error. of excuse"
+        },{
+            question: "Je suis entrain de tomber amoureu(se) de toi !",
+            answer: "I am falling in love with you !",
+            extras: "of for fall"
+        }]
+    }
+];
---
+++ test.js
@@ -0,0 +1,11 @@
+function datas(){
+ let i = 0;
+  
+    while(i<data.length){
+        console.log(data[i].description);
+        i++;
+        
+    }
+}
+datas();
+
